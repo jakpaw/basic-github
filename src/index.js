4a438e827eb3f712c396feadddc99b98138f8a5b
Jakub Pawłot <kjubon@gmail.com>
2019-11-18 21:03:34 +0100

    Add navigation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import main from './styles/main.scss';
 import appHeader from './styles/components/app-header.scss';
+import nav from './styles/components/nav.scss';
+import navItem from './styles/components/nav-item.scss';
