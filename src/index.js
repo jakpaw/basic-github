b6c994883faf267659c53deb659dfd6064c0ed81
Jakub Pawłot <kjubon@gmail.com>
2019-11-18 22:54:23 +0100

    Add list of issues

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import main from './styles/main.scss';
 import appHeader from './styles/components/app-header.scss';
 import nav from './styles/components/nav.scss';
 import navItem from './styles/components/nav-item.scss';
+import issuesList from './styles/components/issues-list.scss';
+import issuesGroup from './styles/components/issues-group.scss';
+import issueCard from './styles/components/issue-card.scss';
